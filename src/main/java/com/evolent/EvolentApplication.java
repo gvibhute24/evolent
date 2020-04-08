f302389132483a2e8895f15826e65eef8732966b
Gangadhar <bba@pixlent.com>
2020-04-08 11:10:39 +0530

    added jar build

--- src/main/java/com/evolent/EvolentApplication.java
+++ src/main/java/com/evolent/EvolentApplication.java
@@ -2,13 +2,13 @@ package com.evolent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+ 
 
 @SpringBootApplication
-public class EvolentApplication extends SpringBootServletInitializer {
+public class EvolentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EvolentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EvolentApplication.class, args);
+    }
 
 }
